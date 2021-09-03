84755f9af5ea8d133569bdc309d29cd1eacb0a4f
mortontaj <87494412+mortontaj@users.noreply.github.com>
2021-09-03 10:53:58 -0400

    Add files via upload

---
+++ Tardis/DivThree.js
@@ -0,0 +1,41 @@
+﻿/// Collaborated with Taj and Yasmine!!
+
+import React, { Component } from "react";
+
+export default class Div3 extends Component{
+    state = {
+        tardis: {
+          name: 'Time and Relative Dimension in Space',
+          caps: false,
+        }
+      }
+    flipIt = (text) => {
+        if (this.state.tardis.caps) {
+          this.setState({
+            tardis: {
+              name: text.toLowerCase(),
+              caps: false
+            }
+          })
+        } else {
+          this.setState({
+            tardis: {
+              name: text.toUpperCase(),
+              caps: true
+            }
+          })
+        }
+      }
+    render() {
+        return (
+            <div>
+                <div>
+                    <h3 onClick={() => this.flipIt(this.state.tardis.name)}>{this.state.tardis.name}</h3>
+                </div>
+                <div>
+                    <h3 onClick={() => this.props.change(this.props.name)}>{this.props.name}</h3>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ Tardis/DivTwo.js
@@ -0,0 +1,14 @@
+﻿/// Collaborated with Silifat and Yasmine!!
+
+import React, { Component } from "react";
+import Div3 from "./DivThree";
+
+export default class Div2 extends Component {
+    render() {
+        return (
+            <div>
+                <Div3 name={this.props.name} change={this.props.change} flip={this.flipIt}/>
+            </div>
+        )
+    }
+}
