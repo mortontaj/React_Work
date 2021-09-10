689ebe8d4b2075bd6e0c59ee822a7490e22e9865
mortontaj <87494412+mortontaj@users.noreply.github.com>
2021-09-09 21:36:36 -0400

    Add files via upload

---
+++ Students_Hw/score.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+// import App from "./App";
+
+export default function Score(props) {
+  return (
+    <div>
+      <div id="date"> Date: {props.score.date}</div>
+      <br />
+      <br />
+      <div id="score">Score: {props.score.score}</div>
+    </div>
+  );
+}
---
+++ Students_Hw/student.js
@@ -0,0 +1,24 @@
+﻿import React from "react";
+import Score from "./score";
+// import App from "./App";
+
+export default function Student(props) {
+  return (
+    <div>
+      <div id="name">Name: {props.student.name}</div>
+      {/* <br />
+      <br /> */}
+      <div id="bio">Bio: {props.student.bio}</div>
+      <br />
+      <br />
+      <div id="scores_list">
+        Scores:{" "}
+        {props.student.scores.map((scores, index) => (
+          <Score score={scores} key={index} />
+        ))}
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+}
